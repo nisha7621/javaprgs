6c67bc3f13f8a55e583f81257c75823cce2545c4
nisha7621 <np922280@gmail.com>
2025-03-25 14:41:20 +0530

    my prgs

---
+++ AssignValue.java
@@ -0,0 +1,9 @@
+class  AssignVariable
+{
+    public static void main(String args[])
+    {
+         int num1=5,num2=10;
+         int sum=num1+num2;
+        System.out.println("Sum="=sum);
+   }
+}
